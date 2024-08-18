a1365c5755632d9579f65f1da3f2bd6a1f385f3f
Julio Cesar Caicedo <llulioscesar@gmail.com>
2024-08-17 21:13:36 -0500

    add type NetworkInterface

--- internal/logs/log.go
+++ internal/logs/log.go
@@ -2,11 +2,33 @@ package logs
 
 import (
 	"github.com/phuslu/log"
+	"os"
+	"strings"
 )
 
 func Init() {
+	var level log.Level
+
+	logLevel := strings.ToUpper(os.Getenv("LOG_LEVEL"))
+	switch logLevel {
+	case "DEBUG":
+		level = log.DebugLevel
+	case "INFO":
+		level = log.InfoLevel
+	case "WARN":
+		level = log.WarnLevel
+	case "ERROR":
+		level = log.ErrorLevel
+	case "FATAL":
+		level = log.FatalLevel
+	case "PANIC":
+		level = log.PanicLevel
+	default:
+		level = log.TraceLevel
+	}
+
 	log.DefaultLogger = log.Logger{
-		Level:      log.InfoLevel,
+		Level:      level,
 		TimeFormat: "15:04:05",
 		Caller:     1,
 		Writer: &log.ConsoleWriter{
